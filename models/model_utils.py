e972b295fe12b3e1305d1342cc20b36f56049343
Chien-Yu Lin <cyulin@cs.washington.edu>
2020-10-07 23:25:31 -0700

    fix model save load path para

--- models/model_utils.py
+++ models/model_utils.py
@@ -1,20 +1,20 @@
 import torch
 import os
 
-def save_model(args, model, filename):
+def save_model(path, model, filename):
     print("Model's state_dict info:")
     for param_tensor in model.state_dict():
         print(param_tensor, "\t", 
                 model.state_dict()[param_tensor].size())
 
-    if not os.path.exists(args.dir):
-        os.makedirs(args.dir)
+    if not os.path.exists(path):
+        os.makedirs(path)
     print("Saving model's state_dict as", filename)
-    torch.save(model.state_dict(), "./{}/{}".format(args.dir, 
+    torch.save(model.state_dict(), "./{}/{}".format(path, 
         filename))
 
-def load_model(args, model, filename):
+def load_model(path, model, filename):
     print("Loading model's state_dict", filename)
-    model.load_state_dict(torch.load("./{}/{}".format(args.dir, 
+    model.load_state_dict(torch.load("./{}/{}".format(path, 
         filename)))
     return model
