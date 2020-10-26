9dc25310ba288b34a73f25f2c51b3a277a62d280
Chien-Yu Lin <cyulin@cs.washington.edu>
2020-10-26 19:07:02 +0000

    update default script for sage

--- models/sage/run_sage_full.sh
+++ models/sage/run_sage_full.sh
@@ -1,10 +1,10 @@
 # able to achieve 96.077% accuracy with mean aggregator on reddit dataset
 
 # training command
-python train_full.py --gpu=0 --dataset=reddit --n-hidden=256 --aggregator-type=gcn --train
+python train_full.py --gpu=0 --dataset=reddit --n-hidden=128 --aggregator-type=mean --train
 
 # inference command for cusparse
-python train_full.py --gpu=0 --dataset=reddit --n-hidden=128 --aggregator-type=gcn --inference
+python train_full.py --gpu=0 --dataset=reddit --n-hidden=128 --aggregator-type=mean --inference
 
 # inference command for cache sample
-# python train_full.py --gpu=0 --dataset=reddit --n-hidden=128 --aggregator-type=gcn --inference --cache-sample
+# python train_full.py --gpu=0 --dataset=reddit --n-hidden=128 --aggregator-type=mean --inference --cache-sample
